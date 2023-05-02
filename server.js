67476d98434f5fc78498d1e34636998def5e928a
Amanda <amanda.sl747@gmail.com>
2023-05-02 12:02:16 +0200

    +

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 import express from "express";
+import bodyParser from "body-parser";
 import cors from "cors";
+import data from "./data.json"
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
@@ -9,12 +11,22 @@ const app = express();
 
 // Add middlewares to enable cors and json body parsing
 app.use(cors());
-app.use(express.json());
+app.use(bodyParser.json());
 
 // Start defining your routes here
 app.get("/", (req, res) => {
   res.send("Hello Technigo!");
 });
+
+app.get('/quotes', (req, res) => {
+  res.json(data)
+})
+
+app.get('/character/:name', (req, res) => {
+  const name = req.params.name
+  const quotesFromCharacter  = data.filter((item) => item.character.house.slug === name)
+  res.json(quotesFromCharacter)
+})
 
 // Start the server
 app.listen(port, () => {
